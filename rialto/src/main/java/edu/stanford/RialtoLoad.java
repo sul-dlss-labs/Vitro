9c15246a709134581594462e648be529272e4086
Joshua Greben <jgreben@stanford.edu>
2018-06-01 14:09:57 -0700

    Use TDB Model with dataset for named graph

--- rialto/src/main/java/edu/stanford/RialtoLoad.java
+++ rialto/src/main/java/edu/stanford/RialtoLoad.java
@@ -2,10 +2,11 @@ package edu.stanford;
 
 import com.hp.hpl.jena.query.Dataset;
 import com.hp.hpl.jena.query.ReadWrite;
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.shared.Lock;
 import com.hp.hpl.jena.sparql.core.Quad;
 import com.hp.hpl.jena.tdb.TDBFactory;
 import com.hp.hpl.jena.tdb.TDBLoader;
-import com.hp.hpl.jena.tdb.sys.TDBInternal;
 
 import java.util.Iterator;
 
@@ -14,16 +15,22 @@ public class RialtoLoad {
     public static void main(String args[]) {
 
         Dataset dataset = TDBFactory.createDataset("/usr/local/vivo/home/tdbContentModels");
+        String graphURI = "http://vitro.mannlib.cornell.edu/default/vitro-kb-2";
+        Model model = dataset.getNamedModel(graphURI);
         dataset.begin(ReadWrite.WRITE);
 
         try {
-            TDBLoader.load(TDBInternal.getBaseDatasetGraphTDB(dataset.asDatasetGraph()), args[0], true);
+            model.enterCriticalSection(Lock.WRITE);
+            TDBLoader.loadModel(model, args[0]);
             dataset.commit();
         } catch (Exception e) {
+            e.printStackTrace();
             dataset.abort();
         } finally {
+            model.leaveCriticalSection();
             dataset.end();
         }
+        System.out.println("=======================================================================================");
 
         dataset.begin(ReadWrite.READ);
         try {
