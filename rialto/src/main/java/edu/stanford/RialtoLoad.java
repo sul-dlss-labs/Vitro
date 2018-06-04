1c6155de1b87a552e1be299cd70b5970fdc6378d
Tommy Ingulfsen <tommyi@stanford.edu>
2018-06-04 16:03:46 -0700

    Use Java 8 and load all the sample data via the TDB API.

--- rialto/src/main/java/edu/stanford/RialtoLoad.java
+++ rialto/src/main/java/edu/stanford/RialtoLoad.java
@@ -8,20 +8,39 @@ import com.hp.hpl.jena.sparql.core.Quad;
 import com.hp.hpl.jena.tdb.TDBFactory;
 import com.hp.hpl.jena.tdb.TDBLoader;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Iterator;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 public class RialtoLoad {
 
     public static void main(String args[]) {
 
-        Dataset dataset = TDBFactory.createDataset("/usr/local/vivo/home/tdbContentModels");
+        // args[0] is expected to be the tcdContentModels directory, e.g.
+        // /usr/local/vitro/home/tdbContentModels       OR    /opt/app/vitro/home/tdbContentModels
+        Dataset dataset = TDBFactory.createDataset();
         String graphURI = "http://vitro.mannlib.cornell.edu/default/vitro-kb-2";
         Model model = dataset.getNamedModel(graphURI);
         dataset.begin(ReadWrite.WRITE);
 
         try {
             model.enterCriticalSection(Lock.WRITE);
-            TDBLoader.loadModel(model, args[0]);
+
+
+            // args[1] is expected to be the agents directory in the sample data set, e.g.
+            // /Users/tommyi/Documents/projects/rialto/github/rialto-etl/sample_data/vivo/agents         OR
+            // /opt/app/vitro/data/rialto-sample-data/vivo/agents
+            List<String> filesToLoad = Files.walk(Paths.get(args[0]))
+                                          .filter(s -> s.toString().endsWith(".nt"))
+                                          .map(Path::toString)
+                                          .sorted()
+                                          .collect(toList());
+
+            TDBLoader.loadModel(model, filesToLoad, true);
             dataset.commit();
         } catch (Exception e) {
             e.printStackTrace();
@@ -30,18 +49,5 @@ public class RialtoLoad {
             model.leaveCriticalSection();
             dataset.end();
         }
-        System.out.println("=======================================================================================");
-
-        dataset.begin(ReadWrite.READ);
-        try {
-            Iterator<Quad> iter = dataset.asDatasetGraph().find();
-            while ( iter.hasNext() ) {
-                Quad quad = iter.next();
-                System.out.println(quad);
-            }
-        } finally {
-            dataset.end();
-        }
-
     }
 }
